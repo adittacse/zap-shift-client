5dcffce0535aa8926fd9df971f8ad410982765a4
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-18 00:23:52 +0600

    logo bug fixed and search implemented in coverage page

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,14 @@
+import { Link } from "react-router";
 import logo from "../../assets/logo.png";
 
 const Logo = () => {
     return (
-        <div className="flex items-end">
-            <img src={logo} alt="logo"/>
-            <h3 className="text-3xl font-extrabold -ms-3">ZapShift</h3>
-        </div>
+        <Link to="/">
+            <div className="flex items-end">
+                <img src={logo} alt="logo"/>
+                <h3 className="text-3xl font-extrabold -ms-3">ZapShift</h3>
+            </div>
+        </Link>
     );
 };
 
--- src/pages/Coverage/Coverage.jsx
+++ src/pages/Coverage/Coverage.jsx
@@ -1,20 +1,58 @@
 import { useLoaderData } from "react-router";
-import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import {useRef} from "react";
 
 const Coverage = () => {
     const data = useLoaderData();
-
+    const mapRef = useRef(null);
     const position = [23.6850, 90.3563];
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        const location = e.target.location.value;
+        const district = data.find(center => center.district.toLowerCase().includes(location.toLowerCase()));
+        if (district) {
+            const coordinate = [district.latitude, district.longitude];
+            mapRef.current.flyTo(coordinate, 12);
+        }
+    }
 
     return (
         <div className="bg-base-100 rounded-4xl py-20 px-[109px] mb-24">
-            <h2 className="font-extrabold text-5xl text-secondary">We are available in 64 districts</h2>
+            <h2 className="font-extrabold text-5xl text-secondary mb-[50px]">We are available in 64 districts</h2>
 
+            {/* search */}
+            <div>
+                <form onSubmit={handleSearch}>
+                    <label className="input">
+                        <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                            <g
+                                strokeLinejoin="round"
+                                strokeLinecap="round"
+                                strokeWidth="2.5"
+                                fill="none"
+                                stroke="currentColor"
+                            >
+                                <circle cx="11" cy="11" r="8"></circle>
+                                <path d="m21 21-4.3-4.3"></path>
+                            </g>
+                        </svg>
+                        <input name="location" type="search" className="grow" placeholder="Search" />
+                    </label>
+                </form>
+            </div>
+
+            <div className="divider my-[50px]"></div>
+
+            <h3 className="text-secondary font-extrabold text-[30px] mb-[30px]">We deliver almost all over Bangladesh</h3>
+
+            {/* map */}
             <div>
                 <MapContainer center={position}
                               zoom={8}
                               scrollWheelZoom={false}
+                              ref={mapRef}
                               className="h-[826px]"
                 >
                     <TileLayer
