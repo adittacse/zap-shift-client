858199c04d8560b36a1e663b29362b59994b42d3
Aditta Chakraborty <cseaditta@gmail.com>
2025-11-21 02:40:35 +0600

    axios secure created and parcel send to the backend

--- src/pages/SendParcel/SendParcel.jsx
+++ src/pages/SendParcel/SendParcel.jsx
@@ -1,19 +1,24 @@
-import React from "react";
+import { useContext } from "react";
 import { useForm, useWatch } from "react-hook-form";
 import { useLoaderData } from "react-router";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../hooks/useAxiosSecure.jsx";
+import AuthContext from "../../contexts/AuthContext/AuthContext.jsx";
 
 const SendParcel = () => {
     const {
         register,
         handleSubmit,
         control,
-        formState: { errors }
+        // formState: { errors }
     } = useForm();
     const serviceCenters = useLoaderData();
     const regionsDuplicate = serviceCenters.map(c => c.region);
     const regions = [...new Set(regionsDuplicate)];
     const senderRegion = useWatch({ control, name: "senderRegion" });
-    const receiverRegion = useWatch({ control, name: "receiverRegion" })
+    const receiverRegion = useWatch({ control, name: "receiverRegion" });
+    const axiosSecure = useAxiosSecure();
+    const { user } = useContext(AuthContext);
 
     const districtsByRegions = (region) => {
         const regionsDistricts = serviceCenters.filter(c => c.region === region);
@@ -22,7 +27,47 @@ const SendParcel = () => {
     }
 
     const handleSendParcel = (data) => {
-        const sameDistrict = data.senderDistrict === data.receiverDistrict;
+        const isDocument = data.parcelType === "document";
+        const isSameDistrict = data.senderDistrict === data.receiverDistrict;
+        const parcelWeight = parseFloat(data.parcelWeight);
+        let cost = 0;
+
+        if (isDocument) {
+            cost = isSameDistrict ? 60 : 80;
+        } else {
+            if (parcelWeight < 3) {
+                cost = isSameDistrict ? 110 : 150;
+            } else {
+                const minCharge = isSameDistrict ? 110 : 150;
+                const extraWeight = parcelWeight - 3;
+                const extraCharge = isSameDistrict ? extraWeight * 40 : extraWeight * 40 + 40;
+                cost = minCharge + extraCharge;
+            }
+        }
+
+        Swal.fire({
+            title: "Are you agree with the Cost?",
+            text: `You will be charged ${cost} taka!`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, I will Pay!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.post("/parcels", data)
+                    .then((res) => {
+                        console.log("res:", res.data);
+                        if (res.data.insertedId) {
+                            Swal.fire({
+                                title: "Congratulations!",
+                                text: "Your parcel has been send.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
     }
 
     return (
@@ -69,11 +114,11 @@ const SendParcel = () => {
                                 <fieldset className="fieldset">
                                     {/* sender name */}
                                     <label className="label">Sender Name</label>
-                                    <input {...register("senderName")} type="text" className="input w-full mb-4" placeholder="Sender Name" />
+                                    <input {...register("senderName")} defaultValue={user?.displayName} type="text" className="input w-full mb-4" placeholder="Sender Name" />
 
                                     {/* sender email */}
                                     <label className="label">Sender Email</label>
-                                    <input {...register("senderEmail")} type="email" className="input w-full mb-4" placeholder="Sender Email" />
+                                    <input {...register("senderEmail")} defaultValue={user?.email} type="email" className="input w-full mb-4" placeholder="Sender Email" />
 
                                     {/* sender phone number */}
                                     <label className="label">Sender Phone Number</label>
