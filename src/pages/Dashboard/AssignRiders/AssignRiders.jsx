fc2439fe5831e48ca459fdd8467d646394e4148c
Aditta Chakraborty <cseaditta@gmail.com>
2025-12-04 16:28:01 +0600

    parcel tracking id send to database

--- src/pages/Dashboard/AssignRiders/AssignRiders.jsx
+++ src/pages/Dashboard/AssignRiders/AssignRiders.jsx
@@ -36,7 +36,8 @@ const AssignRiders = () => {
             riderId: rider._id,
             riderName: rider.riderName,
             riderEmail: rider.riderEmail,
-            parcelId: selectedParcel._id
+            parcelId: selectedParcel._id,
+            trackingId: selectedParcel.trackingId
         };
         axiosSecure.patch(`/parcels/${selectedParcel._id}`, riderAssignInfo)
             .then(res => {
--- src/pages/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
+++ src/pages/Dashboard/AssignedDeliveries/AssignedDeliveries.jsx
@@ -18,7 +18,8 @@ const AssignedDeliveries = () => {
     const handleDeliveryStatusUpdate = (parcel, status) => {
         const statusInfo = {
             deliveryStatus: status,
-            riderId: parcel.riderId
+            riderId: parcel.riderId,
+            trackingId: parcel.trackingId
         };
 
         let message = `Parcel status is update with ${status.split("_").join(" ")}.`;
